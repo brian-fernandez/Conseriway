2ac8978606bd2d9509183859060c1ba8d0eef7a7
brian Fernandez Mercado <nsklobuchar666@gmail.com>
2022-07-13 18:07:23 -0400

    v2

---
+++ js/navback.js
@@ -0,0 +1,89 @@
+$('.fixed-top').addClass('bg-dark');
+$('.titulo').addClass('text-light shadow');
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-dark shadow');
+                $('.titulo').addClass('text-light shadow');
+                $('.titulo').removeClass('text-dark shadow');
+            } else {
+                
+                $('.titulo').addClass('text-light shadow');
+                
+            }
+        } else {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-dark shadow').css('top', -45);
+                $('.titulo').addClass('text-light shadow');
+                $('.titulo').removeClass('text-dark ');
+                
+            } else {
+                $('.fixed-top').removeClass('shadow').css('top', 0);
+                
+            }
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 25,
+        loop: true,
+        center: true,
+        dots: false,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
+    });
+
+    
+})(jQuery);
+
